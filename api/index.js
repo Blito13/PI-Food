80d13a288df4b53f05d0e70a671c466fa39832a3
Blito13 <lesta4444@hotmail.com>
2022-11-24 19:19:25 -0300

    trc

--- api/index.js
+++ api/index.js
@@ -19,7 +19,8 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn  } = require('./src/db.js');
-
+const port =  process.env.PORT
+console.log(port)
 // Syncing all the models at once.
 conn.sync({ force:false}).then(() => {
   server.listen(process.env.PORT, () => {
