b442d597ef4f6626b2cf24bd06d0989bb574e7cd
Blito13 <lesta4444@hotmail.com>
2022-11-29 22:07:10 -0300

    ll

--- client/src/Components/LandingP.jsx
+++ client/src/Components/LandingP.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 import estilos from './LandingP.module.css' 
-const lof = process.env.REACT_APP_API;
 
+console.log(process.env.REACT_APP_BE_URL)
 export default function LandingPage(){
-    console.log(lof)
+   
     return(
         <div className={estilos.contenedor}>
             <h1 className={estilos.titulo}>Recipedia</h1>
