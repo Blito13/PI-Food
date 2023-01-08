a9dd40576dbc982437f49a6bafe5f4acc60ac332
Blito13 <lesta4444@hotmail.com>
2023-01-08 12:31:44 -0300

    store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css';
+import generateStore from "./Redux/store";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import { Provider } from "react-redux"; 
-import store from "./Redux/store";
+/* import store from "./Redux/store"; */
 import LandingP from './Components/LandingP.jsx'
 import Home from './Components/Home.jsx'
 import Detail from './Components/Detail.jsx'
 import RecipeCreate from './Components/RecipeCreate.jsx'
 import Modal from './Components/Modal';
 function App() {
- 
+ const store = generateStore();
   return (
     <Provider store={store}>
       <div className="App">
--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,11 +1,22 @@
-import { createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootR from '../Redux/reducers';
 
-export const store = createStore(
+
+/* export const store = createStore(
     rootR,
     composeWithDevTools(applyMiddleware(thunk))
     );
 
-export default store; 
+export default store;  */
+import {combineReducers,compose,applyMiddleware, legacy_createStore} from "redux"
+import thunk from "redux-thunk"
+import rootR from "./reducers/reducers"
+
+const reducer = combineReducers ({
+    rootR,
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export default function generateStore(){
+    const store = legacy_createStore(rootR,composeEnhancers(applyMiddleware(thunk)))
+    return store;
+}
